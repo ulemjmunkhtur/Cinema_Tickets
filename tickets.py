8dee336eadbf0c91533432e5314b8178e446ccd8
ulemjmunkhtur <98507788+ulemjmunkhtur@users.noreply.github.com>
2022-03-23 13:46:37 +0800

    list of movies

--- tickets.py
+++ tickets.py
@@ -1,1 +1,21 @@
+print('Welcome to Prime Cineplex Encanto')
+name = input ('What is your name? ')
+phone_number = input('What is your phone number? ')
+
+print("These are today's movies")
+movies = ['Encanto','Forrest Gump', 'Avatar the Last Airbender']
+
+for count, movie in enumerate(movies):
+    print (f"{count + 1}. {movie}")
+
+movie_selection = None
+
+while movie_selection not in range(1,4):
+    try: 
+        movie_selection = int(input('Please, enter your choice of movie from 1-3: '))
+    except:
+        print ("Nope")
+
+print('you have chosen', movies[movie_selection-1])
+
 
