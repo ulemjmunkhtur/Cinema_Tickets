c7012b760cc11a6c3508d78660bd90f2d2ac84fb
ulemjmunkhtur <98507788+ulemjmunkhtur@users.noreply.github.com>
2022-04-06 13:57:58 +0800

    Mr. V's Comments

--- tickets.py
+++ tickets.py
@@ -1,6 +1,7 @@
 print('Welcome to Prime Cineplex Encanto')
 name = input ('What is your name? ')
 phone_number = input('What is your phone number? ')
+#validate phone number#
 
 print("These are today's movies")
 movies = ['Encanto','Forrest Gump', 'Avatar the Last Airbender']
@@ -18,4 +19,9 @@ while movie_selection not in range(1,4):
 
 print('you have chosen', movies[movie_selection-1])
 
+session = input(f"Which session would you like to watch {movies[movie_selection-1]} in? Morning, Afternoon, or Evening?")
+print("you chose the", session, "session")
 
+#MATCH SESSION TO LOWERCASE OR UPPERCASE. lOWERCASE WITH THE KEY 
+
+print("Okay. You have chosen to watch", movies[movie_selection-1])
